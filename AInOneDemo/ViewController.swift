faef74a949a6cf227af140ce1fe0a5750d2fbb6f
Puja Rathod <puja.r@cearsinfotech.com>
2019-06-13 20:30:02 +0530

    Initial Commit

---
+++ AInOneDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AInOneDemo
+//
+//  Created by Puja Rathod on 13/06/19.
+//  Copyright © 2019 Puja Rathod. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
