66dd7549be9235edd7e896b9b3629972bf7a8bd4
Puja Rathod <puja.r@cearsinfotech.com>
2019-06-14 09:11:20 +0530

    upload

--- AInOneDemo/ViewController.swift
+++ AInOneDemo/ViewController.swift
@@ -106,3 +106,101 @@ extension ViewController {
     }
 }
 
+extension ViewController {
+    func setDashboardViewController()
+    {
+//        let VC = HomeViewController(nibName: "HomeViewController", bundle: nil)
+//        appDelegate.navigationController = UINavigationController(rootViewController: VC)
+//        appDelegate.navigationController?.navigationBar.isHidden = true
+//        appDelegate.window?.rootViewController = appDelegate.navigationController
+//        appDelegate.window?.makeKeyAndVisible()
+    }
+    
+    func callSignUpAPI() {
+//        var param = [String:AnyObject]()
+//        param["User_FirstName"] = txtFname.text as AnyObject
+//        param["User_LastName"] = txtSerName.text as AnyObject
+//        param["User_Email"] = txtEmail.text as AnyObject
+//        param["User_Password"] = txtPass.text as AnyObject
+//        param["Device_Token"] = getUserDefaultValue("Token") as AnyObject?
+//        param["Device_Type"] = DEVICE_TYPE as AnyObject
+//        param["agreementText_Id"] = self.GDPRVersion as AnyObject
+//        
+//        ShowLoaderOnView()
+//        Alamofire.upload(multipartFormData: { (multipartFormData) in
+//            
+//            for (key,value) in param {
+//                multipartFormData.append((value.data(using: String.Encoding.utf8.rawValue, allowLossyConversion: false))!, withName: key)
+//            }
+//            
+//            if(self.image != nil){
+//                if let image1 = UIImageJPEGRepresentation(self.image!, 0.4) {
+//                    multipartFormData.append(image1, withName: "User_Profile", fileName: "profile.png", mimeType: "image/jpg")
+//                }
+//            }
+//            
+//        }, to: URL_SIGNUP, encodingCompletion: { (result) in
+//            // code
+//            switch result {
+//            case .success(request: let upload, streamingFromDisk: _, streamFileURL: _):
+//                upload.validate().responseJSON {
+//                    response in
+//                    HideLoaderOnView()
+//                    if response.result.isFailure {
+//                        debugPrint(response)
+//                    } else {
+//                        let result = response.value as! NSDictionary
+//                        print(result)
+//                        if(result.object_forKeyWithValidationForClass_Int("success") == 1){
+//                            if let dicResp = result.value(forKey: "response") as? [String : AnyObject] {
+//                                AIUser.sharedManager.setvalueUserWithdetails(dict:dicResp)
+//                                if let isUsrVerified = dicResp["is_verified"] as? Int {
+//                                    if isUsrVerified == 1 {
+//                                        //Store popup
+////                                        AIUser.sharedManager.saveInDefaults()
+////                                        CommonUtils.shared().setDashboardViewController()
+//                                    } else {
+//                                        if let strUID = dicResp["User_Id"] as? String {
+//                                            self.showOTPPopUp(strOtp: dicResp["otp"]!.description!, strUId: strUID)
+//                                        }
+//                                    }
+//                                }
+//                            } else {
+//                                if let dicResp = result.value(forKey: "response") as? [String : AnyObject] {
+//                                    if let strUID = dicResp["User_Id"] as? String,
+//                                        let intValue = Int(strUID),
+//                                        intValue > 0 {
+//                                        displayAlertWithTitle(APP_NAME, andMessage: result.object_forKeyWithValidationForClass_String("message"), buttons: ["Ok"], completion: { (index) in
+//                                            self.navigationController?.popViewController(animated: true)
+//                                        })
+//                                    }
+//                                } else {
+//                                    displayAlertWithMessage(result.object_forKeyWithValidationForClass_String("message"))
+//                                }
+//                            }
+//                        } else {
+//                            
+//                            if let dicResp = result.value(forKey: "response") as? [String : AnyObject] {
+//                                if let strUID = dicResp["User_Id"] as? String,
+//                                    let intValue = Int(strUID),
+//                                    intValue > 0 {
+//                                    displayAlertWithTitle(APP_NAME, andMessage: result.object_forKeyWithValidationForClass_String("message"), buttons: ["Ok"], completion: { (index) in
+//                                        //                                        self.navigationController?.popViewController(animated: true)
+//                                    })
+//                                }
+//                            } else {
+//                                displayAlertWithMessage(result.object_forKeyWithValidationForClass_String("message"))
+//                            }
+//                        }
+//                    }
+//                }
+//            case .failure(let encodingError):
+//                HideLoaderOnView()
+//                NSLog((encodingError as NSError).localizedDescription)
+//            }
+//        })
+//        
+    }
+    
+    
+}
